607013c39129833e89ce6c3f884b41381381d2bc
Prateek Tandon <prateek1.tandon@gmail.com>
2023-03-23 00:05:12 -0700

    fix

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 setuptools.setup(
-    name="pokemon-card-recognizer",
-    version="0.0.1.3.5",
+    name="pokemon-go-video-analysis",
+    version="0.0.0.0.1",
     author="Prateek Tandon",
     author_email="prateek1.tandon@gmail.com",
-    description="Pokemon TCG Card Recognizer",
+    description="Pokemon Go Video Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/prateekt/pokemon-card-recognizer",
+    url="https://github.com/prateekt/pokemon-go-video-analysis",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
